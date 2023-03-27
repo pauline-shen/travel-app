898a9947ea1fe37bd631621ae12e70f6cae7a4b5
pauline-shen <s9633291@gmail.com>
2023-03-27 00:57:25 -0400

    edit files and setup

--- db/connect.js
+++ db/connect.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const { USER, PASSWORD, HOST, DATABASE } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const pool = new Pool({
-  user: USER,
-  password: PASSWORD,
-  host: HOST,
-  database: DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  database: DB_NAME,
+  port: DB_PORT,
 });
+
 const connectDB = () => {
   return pool
     .connect()
     .then(() =>
-      console.log(`The application is connected to ${DATABASE} database`)
+      console.log(`The application is connected to ${DB_NAME} database`)
     )
     .catch((err) => console.error("connection error", err));
 
